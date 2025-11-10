69aafe0a6b4651db4ea21e3d9e140036645837db
Shangyu YANG <shangyu.yang@eleve.isep.fr>
2025-11-10 10:50:18 +0100

    Green

--- primechecker/src/main/java/com/example/PrimeChecker.java
+++ primechecker/src/main/java/com/example/PrimeChecker.java
@@ -1,9 +1,33 @@
 package com.example;
 
+// public class PrimeChecker {
+
+//     public boolean isPrime(int n) {
+//         // Red）
+//         return false;
+//     }
+// }
+
+//Green
 public class PrimeChecker {
 
     public boolean isPrime(int n) {
-        // Red）
-        return false;
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+      
+        if (n % 2 == 0) {
+            return false;
+        }
+        
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
